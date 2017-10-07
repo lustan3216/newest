bd0d35d0b5850cc98b58072c0b4415511e0c99fa
lululala <et3216@gmail.com>
2017-10-07 22:35:38 +0800

    serialize_data in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  def serialize_data(data, serializer, options = {})
+    if data.respond_to?(:to_ary)
+      data.map { |record| serializer.new(record, options).as_json }
+    else
+      serializer.new(data).as_json
+    end
+  end
 end
