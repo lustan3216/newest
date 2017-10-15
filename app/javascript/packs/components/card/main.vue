10c769294706c6d59d16fce243263ed417bec6b6
lululala <et3216@gmail.com>
2017-10-15 21:42:12 +0800

    add lodash

--- app/javascript/packs/components/card/main.vue
+++ app/javascript/packs/components/card/main.vue
@@ -31,33 +31,35 @@
 </template>
 
 <script>
-  import timeMixin   from '../../mixins/time'
+  import timeMixin from '../../mixins/time'
+  import _         from 'lodash'
   
   export default {
     props: ['website'],
     mixins: [timeMixin],
     methods: {
       deleteWebsite(e){
-        const id = $(e.target).parents('.website-card').data('id');
+        const id = $(e.target).parents('.website-card').data('id')
         
         this.axios.delete(`/websites/${+id}`)
           .then(() => {
-            this.$parent.fetch_websites();
+          
+            this.$parent.fetch_websites()
             Materialize.toast('刪除成功', 4000)
           })
       },
       setCurrentEpisode(event){
-        event.preventDefault();
-        const episode = event.target.dataset.id;
-        const id = $(event.target).parents('.website-card').data('id');
+        event.preventDefault()
+        const episode = event.target.dataset.id
+        const id      = $(event.target).parents('.website-card').data('id')
         
         this.axios.put(`/websites/${id}/current_episode`, {
             episode
           })
           .then(({data: {messages}}) => {
-            if (messages) return Materialize.toast(messages, 4000);
+            if (messages) return Materialize.toast(messages, 4000)
 
-            this.$parent.fetch_websites();
+            this.$parent.fetch_websites()
             Materialize.toast('新增成功', 4000)
           })
       }
