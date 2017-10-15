a41462c2b009f46d5c6bc4a9ba964a0a68a8ab0a
lululala <et3216@gmail.com>
2017-10-15 20:29:29 +0800

    pack application refactor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,10 @@
-import Vue    from 'vue'
+import Vue          from 'vue'
+import axios        from 'axios'
+import VueAxios     from 'vue-axios'
 import websiteIndex from './websites/index'
+
+Vue.use(VueAxios, axios);
+axios.defaults.headers.common['X-CSRF-Token'] = $('meta[name="csrf-token"]').attr('content')
 
 new Vue({
   el: '#app',
