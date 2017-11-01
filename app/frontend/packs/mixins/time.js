cf2179148ffcdb68aba11dca76a8e6d372a9a240
lululala <et3216@gmail.com>
2017-11-01 08:00:22 +0800

    file moving

---
+++ app/frontend/packs/mixins/time.js
@@ -0,0 +1,9 @@
+import moment      from 'moment/min/moment-with-locales';
+
+export default {
+    filters:{
+        fromNow(datetime){
+            return moment(datetime).fromNow();
+        }
+    }
+}
--- app/javascript/packs/mixins/time.js
+++
@@ -1,9 +0,0 @@
-import moment      from 'moment/min/moment-with-locales';
-
-export default {
-  filters:{
-    fromNow(datetime){
-      return moment(datetime).fromNow();
-    }
-  }
-}
