ddd41989c50b7d9db5688db5c775dd9c1ac1d7f2
lululala <et3216@gmail.com>
2017-11-01 08:02:57 +0800

    file moving

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,11 +3,15 @@ import Vue          from 'vue'
 import axios        from 'axios'
 import VueAxios     from 'vue-axios'
 import websiteIndex from './websites/index'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
+Vue.use(iView);
 Vue.use(VueAxios, axios);
+
 axios.defaults.headers.common['X-CSRF-Token'] = $('meta[name="csrf-token"]').attr('content')
 
 new Vue({
-  el: '#app',
-  mixins: [ websiteIndex ]
+    el: '#app',
+    mixins: [ websiteIndex ]
 });
