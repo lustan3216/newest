58743f455dfe07b9ecc40177c01c31eda6868142
lululala <et3216@gmail.com>
2017-11-07 18:00:56 +0800

    fix rdb website prefix

--- app/services/rdb/website.rb
+++ app/services/rdb/website.rb
@@ -1,10 +1,11 @@
 class Rdb::Website
 
-  attr_reader :main_url, :redis
+  attr_reader :main_url, :redis, :keyword
 
-  def initialize(main_url)
+  def initialize(website)
     @redis    = $redis
-    @main_url = main_url
+    @main_url = website.url
+    @keyword  = website.keyword
   end
 
   # sub-url / sub-url-title / main-url = { 'episodes1': 'url1',  'episodes2': 'url2' }
@@ -22,31 +23,31 @@ class Rdb::Website
   end
 
   def sub_url_exist?(key)
-    redis.hexists("#{main_url}:sub-url-title", key)
+    redis.hexists("#{keyword}:#{main_url}:sub-url-title", key)
   end
 
   def sub_url_title(key)
-    redis.hget("#{main_url}:sub-url-title", key)
+    redis.hget("#{keyword}:#{main_url}:sub-url-title", key)
   end
 
   def sub_urls_title
-    redis.hgetall("#{main_url}:sub-url-title")
+    redis.hgetall("#{keyword}:#{main_url}:sub-url-title")
   end
 
   def add_sub_url_title(hash)
-    redis.hset("#{main_url}:sub-url-title", hash.keys[0], hash.values[0])
+    redis.hset("#{keyword}:#{main_url}:sub-url-title", hash.keys[0], hash.values[0])
   end
 
   def sub_url(key)
-    redis.hget("#{main_url}:sub-url", key)
+    redis.hget("#{keyword}:#{main_url}:sub-url", key)
   end
 
   def sub_urls
-    redis.hgetall("#{main_url}:sub-url")
+    redis.hgetall("#{keyword}:#{main_url}:sub-url")
   end
 
   def add_sub_url(hash)
-    redis.hset("#{main_url}:sub-url", hash.keys[0], hash.values[0])
+    redis.hset("#{keyword}:#{main_url}:sub-url", hash.keys[0], hash.values[0])
   end
 
 end
