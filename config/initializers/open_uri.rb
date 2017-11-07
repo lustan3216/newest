d7b51c6732063eb3dc268e48296eaad1e853f57b
lululala <et3216@gmail.com>
2017-11-07 23:43:31 +0800

    open uri initilzizer

--- config/initializers/open_uri.rb
+++ config/initializers/open_uri.rb
@@ -1,4 +1,8 @@
 require 'open-uri'
 require 'openssl'
-CrawlerUserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+OPENURL_SETTING = {
+  'User-Agent'=> 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+  ssl_verify_mode: 0
+}
+
