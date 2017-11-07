0506b519b1764c54e3986159124815402e78530a
lululala <et3216@gmail.com>
2017-11-07 17:58:19 +0800

    website controller set check keyword_url

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -11,6 +11,24 @@ class WebsitesController < ApplicationController
 
   def create
     website = current_user.websites.new(website_params)
+
+    if current_user.websites.find_by_url(website_params[:url])
+      render json: { messages: 'already have' } and return
+    end
+
+    if website_params[:keyword_url].blank?
+      keyword_urls = Crawler::Website.new(website).keyword_urls
+
+      case keyword_urls.to_a.size
+      when 0
+        render json: { data: keyword_urls, type: 'not_found' } and return
+      when 1
+        website.keyword_url = keyword_urls.first[:url]
+      else
+        render json: { data: keyword_urls, type: 'keyword_urls' } and return
+      end
+    end
+
     if website.save
       render json: serialize_data(website, WebsiteSerializer)
     else
@@ -25,6 +43,6 @@ class WebsitesController < ApplicationController
   private
 
   def website_params
-    params.require(:website).permit(:url, :keyword)
+    params.require(:website).permit(:url, :keyword, :pattern, :keyword_url)
   end
 end
